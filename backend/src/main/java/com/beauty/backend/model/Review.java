024ddccd0b96a62d6be3c62c5e9005689f48372f
Rutuja Tayade <rutujatayde410@gmail.com>
2026-05-30 12:35:53 +0530

    added review section

--- backend/src/main/java/com/beauty/backend/model/Review.java
+++ backend/src/main/java/com/beauty/backend/model/Review.java
@@ -28,4 +28,52 @@ public class Review {
     }
 
     // Generate Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public LocalDateTime getReviewDate() {
+        return reviewDate;
+    }
+
+    public void setReviewDate(LocalDateTime reviewDate) {
+        this.reviewDate = reviewDate;
+    }
 }
